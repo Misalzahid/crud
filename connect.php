643b25dd2b9a41ccbf3f1d01c2bf0ce7edf2fe97
Misalzahid <143399501+Misalzahid@users.noreply.github.com>
2023-12-05 14:35:08 +0500

    adding files

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$con=new mysqli('localhost','root','','crudopertion' );
+
+if (!$con)
+{
+    die(mysqli_error($con));
+}
+
+?>
---
+++ display.php
@@ -0,0 +1,73 @@
+<?php 
+ include 'connect.php';
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Crud Opertion</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  </head>
+  <body>
+
+   <div class="container">
+    <button class ="btn btn-primary my-5" > <a href="user.php"
+    class="text-light"> Add user</a>
+    </button>
+
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Sl no</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Mobile</th>
+      <th scope="col">Password</th>
+      <th scope="col">Opertions</th>
+
+    </tr>
+  </thead>
+  <tbody>
+<?php 
+
+$sql="Select * from crud";
+$result=mysqli_query($con,$sql);
+if($result){
+
+    while( $row=mysqli_fetch_assoc($result)){
+        $id=$row['id'];
+        $name=$row['name'];
+        $email=$row['email'];
+        $mobile=$row['mobile'];
+        $password=$row['password'];
+        echo'<tr>
+        <th scope="row">'.$id.'</th>
+        <td>'.$name.'</td>
+        <td>'.$email.'</td>
+        <td>'.$mobile.'</td>
+        <td>'.$password.'</td>
+        <td>
+        <button class ="btn btn-primary"><a href="update.php? 
+        updateid='.$id.'"class="text-light">Update</a></button>
+        <button class ="btn btn-danger"><a href="delete.php?
+        deleteid='.$id.'" class="text-light">Delete</a></button>
+        </td>
+      </tr>';
+    }
+}
+
+?>
+ 
+</tbody>
+</table>
+   </div>
+
+</body>
+</html>
